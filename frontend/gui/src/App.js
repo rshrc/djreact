dd3cdd39a04e2aee693a3e560eead091adc9efc4
rshrc <rishieric91@gmail.com>
2018-09-29 21:55:09 +0530

    React Auth Part Partially Written

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -3,13 +3,19 @@ import CustomLayout from "./containers/Layout";
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 import { BrowserRouter as Router } from "react-router-dom";
 import BaseRouter from "./routes";
+import { connect } from "react-redux";
+import * as actions from "./store/actions/auth";
 
 class App extends Component {
+  componentDidMount() {
+    this.props.onTryAutoSignup;
+  }
+
   render() {
     return (
-      <div className="App">
+      <div>
         <Router>
-          <CustomLayout>
+          <CustomLayout {...this.props}>
             <BaseRouter />
           </CustomLayout>
         </Router>
@@ -18,4 +24,19 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state != null
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);

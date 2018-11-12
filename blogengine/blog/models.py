b79c50f682ac3fcdfdb9f3ce6a7ca2f97e182517
rshrc <rishieric91@gmail.com>
2018-11-13 02:33:28 +0530

    Added django_taggit

--- blogengine/blog/models.py
+++ blogengine/blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.urls import reverse
 # Create your models here.
 from django.utils import timezone
-
+from taggit.managers import TaggableManager
 
 class PublishedManager(models.Manager):
 
@@ -14,6 +14,7 @@ class PublishedManager(models.Manager):
 
 
 class Post(models.Model):
+    tags = TaggableManager()
     STATUS_CHOICES = (
         ('draft', 'Draft'),
         ('published', 'Published'),

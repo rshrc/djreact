15a275f21f42271b6883f6f37aa8076807d95201
rshrc <rishieric91@gmail.com>
2018-11-13 02:30:10 +0530

    Added Comment Feature

--- blogengine/blog/models.py
+++ blogengine/blog/models.py
@@ -1,21 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 from django.utils import timezone
-from django.contrib.auth.models import User
-from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
 
     def get_queryset(self):
         return super(PublishedManager,
-                     self).get_queryset()\
+                     self).get_queryset() \
             .filter(status='published')
 
 
 class Post(models.Model):
-
     STATUS_CHOICES = (
         ('draft', 'Draft'),
         ('published', 'Published'),
@@ -49,3 +47,19 @@ class Post(models.Model):
                              self.publish.month,
                              self.publish.day,
                              self.slug])
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return 'Comment by {} on {}'.format(self.name, self.post)

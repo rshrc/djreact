8a453b97d37bfa162b95d91894f5129a2c2ca8d8
rshrc <rishieric91@gmail.com>
2018-11-13 02:43:38 +0530

    Completed Tagging Page 125

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -2,7 +2,7 @@ from django.core.mail import send_mail
 from django.core.paginator import PageNotAnInteger, Paginator, EmptyPage
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
-
+from taggit.models import Tag
 from .forms import EmailPostForm, CommentForm
 from .models import Post
 
@@ -16,8 +16,14 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html'
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
     paginator = Paginator(object_list, 3)
     page = request.GET.get('page')
     try:
@@ -28,7 +34,7 @@ def post_list(request):
     except EmptyPage:
         # If page is out of range deliver the first page
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts, 'tag': tag})
 
 
 def post_detail(request, year, month, day, post):

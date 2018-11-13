152c3724a139f20d8a08766da987161488fb929b
rshrc <rishieric91@gmail.com>
2018-11-13 14:47:38 +0530

    Added Markdown and fixed Renderer

--- blogengine/blog/admin.py
+++ blogengine/blog/admin.py
@@ -6,21 +6,21 @@ from .models import Post, Comment
 # Register your models here.
 
 
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'author', 'publish',
-                    'status')
-    list_filter = ('status', 'created', 'publish', 'author')
+admin.site.register(Post)
+# class PostAdmin(admin.ModelAdmin):
+#     list_display = ('title', 'slug', 'author', 'publish',
+#                     'status')
+#     list_filter = ('status', 'created', 'publish', 'author')
+#
+#     search_fields = ('title', 'body')
+#     prepopulated_fields = {'slug': ('title',)}
+#     raw_id_fields = ('author',)
+#     date_hierarchy = 'publish'
+#     ordering = ('status', 'publish')
 
-    search_fields = ('title', 'body')
-    prepopulated_fields = {'slug': ('title',)}
-    raw_id_fields = ('author',)
-    date_hierarchy = 'publish'
-    ordering = ('status', 'publish')
 
-
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'post', 'created', 'active')
-    list_filter = ('active', 'created', 'updated')
-    search_field = ('name', 'email', 'body')
+admin.site.register(Comment)
+# class CommentAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'email', 'post', 'created', 'active')
+#     list_filter = ('active', 'created', 'updated')
+#     search_field = ('name', 'email', 'body')
--- blogengine/blog/forms.py
+++ blogengine/blog/forms.py
@@ -7,7 +7,7 @@ class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
     to = forms.EmailField()
-    comments = forms.CharField(required=False, widget=forms.Textarea)
+    comments = forms.CharField(required=False, widget=forms.CharField)
 
 
 class CommentForm(forms.ModelForm):

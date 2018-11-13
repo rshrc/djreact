0de55b1ceaceabd3838d4171353cb44885a951bf
rshrc <rishieric91@gmail.com>
2018-11-13 18:32:59 +0530

    Integrated Trigamality

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,10 +1,11 @@
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from taggit.models import Tag
+
 
 from .forms import EmailPostForm, CommentForm, SearchForm
 from .models import Post
@@ -111,14 +112,14 @@ def post_search(request):
     form = SearchForm()
     query = None
     results = []
-    search_vector = SearchVector('title', 'body')
+    search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
     search_query = SearchQuery(query)
     if 'query' in request.GET:
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
             results = Post.objects.annotate(
-                search=search_vector, rank=SearchRank(search_vector, search_query),
-            ).filter(search=search_query).order_by('-rank')
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.3).order_by('-similarity')
 
     return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})

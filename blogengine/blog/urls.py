0c1df7785df266f87bb93a2c8036e5a1af8bfe71
rshrc <rishieric91@gmail.com>
2018-11-13 16:47:14 +0530

    Successfully Created SearchEngine for Blog

--- blogengine/blog/urls.py
+++ blogengine/blog/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          views.post_detail,
          name='post_detail'),
+    path('search/', views.post_search, name='post_search'),
     path('<int:post_id>/share/',
          views.post_share, name='post_share'),
     path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),

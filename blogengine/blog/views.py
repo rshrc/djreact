5a3b5f2ae79e3716fd5e3650f7a61e561403ec54
rshrc <rishieric91@gmail.com>
2018-11-13 16:52:24 +0530

    Enabled Searching by Weightage

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,4 +1,4 @@
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Count
@@ -111,12 +111,14 @@ def post_search(request):
     form = SearchForm()
     query = None
     results = []
+    search_vector = SearchVector('title', 'body')
+    search_query = SearchQuery(query)
     if 'query' in request.GET:
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
             results = Post.objects.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
+                search=search_vector, rank=SearchRank(search_vector, search_query),
+            ).filter(search=search_query).order_by('-rank')
 
     return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})

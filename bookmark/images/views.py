ab6a52a7b3a8809931223e53bbaac5de9140520f
rshrc <rishieric91@gmail.com>
2018-11-16 16:52:39 +0530

    Added Image Like/Unlike Option without jQuery

--- bookmark/images/views.py
+++ bookmark/images/views.py
@@ -1,13 +1,33 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST
 
 from .forms import ImageCreationForm
 from .models import Image
 
 
 # Create your views here.
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except:
+            pass
+    return JsonResponse({'status': 'ko'})
+
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)

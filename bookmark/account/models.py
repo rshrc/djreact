ed5c3430f6e7d3fa8881d8766db9df51c1a22ca6
rshrc <rishieric91@gmail.com>
2018-11-17 19:25:58 +0530

    Adding Ajax Follow View

--- bookmark/account/models.py
+++ bookmark/account/models.py
@@ -1,12 +1,14 @@
 from django.conf import settings
+from django.contrib.auth.models import User
 from django.db import models
 
 
 # Create your models here.
 
+
 class Contact(models.Model):
     user_form = models.ForeignKey('auth.User', related_name='rel_from_set', on_delete=models.CASCADE)
-    user_to = models.ForeignKey('auth.User', related_name='rel_from_set', on_delete=models.CASCADE)
+    user_to = models.ForeignKey('auth.User', related_name='rel_to_set', on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True, db_index=True)
 
     class Meta:
@@ -23,3 +25,6 @@ class Profile(models.Model):
 
     def __str__(self):
         return 'Profile for user {}'.format(self.user.username)
+
+# Add following field to User Dynamically
+User.add_to_class('following', models.ManyToManyField('self', through=Contact, related_name='followers', symmetrical=False))

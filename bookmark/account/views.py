7438d5ecde75e95dc6ae5cb407af3c9aa7eda642
rshrc <rishieric91@gmail.com>
2018-11-17 19:37:08 +0530

    Made the Follow/Unfollow View

--- bookmark/account/views.py
+++ bookmark/account/views.py
@@ -1,11 +1,14 @@
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.contrib import messages
-from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render
+from django.views.decorators.http import require_POST
+
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
+from .models import Contact
 
 
 def user_login(request):
@@ -75,4 +78,27 @@ def user_list(request):
 def user_detail(request, username):
     user = get_object_or_404(User, username=username, is_active=True)
 
-    return render(request, 'account/user/detail.html', {'section':'people', 'user': user})
+    return render(request, 'account/user/detail.html', {'section': 'people', 'user': user})
+
+
+@login_required
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(
+                    user_from=request.user,
+                    user_to=user
+                )
+            else:
+                Contact.objects.filter(user_from=request.user, user_to=user).delete()
+
+            return JsonResponse({'status': 'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status': 'ok'})
+    return JsonResponse({'status': 'ko'})

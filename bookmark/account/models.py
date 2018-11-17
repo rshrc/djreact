ffe2e3f3b4d63d7a37f49caa151aa9ae92628fbf
rshrc <rishieric91@gmail.com>
2018-11-17 18:35:45 +0530

    Created Contact Model

--- bookmark/account/models.py
+++ bookmark/account/models.py
@@ -4,6 +4,18 @@ from django.db import models
 
 # Create your models here.
 
+class Contact(models.Model):
+    user_form = models.ForeignKey('auth.User', related_name='rel_from_set', on_delete=models.CASCADE)
+    user_to = models.ForeignKey('auth.User', related_name='rel_from_set', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return '{} follows {}'.format(self.user_form, self.user_to)
+
+
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)

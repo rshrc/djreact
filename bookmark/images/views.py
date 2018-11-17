28ad459f51c05bfdf4948839c9396cf5cca1a0f8
rshrc <rishieric91@gmail.com>
2018-11-18 02:18:38 +0530

    Completed Making a social engine

--- bookmark/images/views.py
+++ bookmark/images/views.py
@@ -45,7 +45,10 @@ def image_detail(request, id, slug):
     # increment total image views by 1
     total_views = r.incr('image:{}:views'.format(image.id))
 
-    return render(request, 'images/image/detail.html', {'section': 'images', 'image': image, 'total_views': total_views})
+    # increment image ranking by 1
+    r.zincrby('image_ranking', image.id, 1)
+    return render(request, 'images/image/detail.html',
+                  {'section': 'images', 'image': image, 'total_views': total_views})
 
 
 @login_required
@@ -90,3 +93,16 @@ def image_list(request):
         return render(request, 'images/image/list_ajax.html', {'section': 'images', 'images': images})
 
     return render(request, 'images/image/list.html', {'section': 'images', 'images': images})
+
+
+@login_required
+def image_ranking(request):
+    # get image ranking dictionary
+    image_ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+
+    # get most viewed images
+    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(request, 'images/image/ranking.html', {'section': 'images', 'most_viewed': most_viewed})

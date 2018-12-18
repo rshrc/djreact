9ddba559d0fcb0f643bb8025e7a2b0a3892a866a
rshrc <rishieric91@gmail.com>
2018-12-18 15:56:40 +0530

    Stuck with issues

--- online_shop/cart/views.py
+++ online_shop/cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.http import require_POST
 
 from shop.models import Product
@@ -8,9 +8,9 @@ from .forms import CartAddProductForm
 
 # Create your views here.
 @require_POST
-def cart_add(request, produt_id):
+def cart_add(request, product_id):
     cart = Cart(request)
-    product = get_object_or_404(Product, id=produt_id)
+    product = get_object_or_404(Product, id=product_id)
     form = CartAddProductForm(require_POST)
 
     if form.is_valid():
@@ -26,6 +26,11 @@ def cart_remove(request, product_id):
     cart.remove(product)
     return redirect('cart:cart_detail')
 
+
 def cart_detail(request):
     cart = Cart(request)
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(
+            initial={'quantity': item['quantity'], 'update': True}
+        )
     return render(request, 'cart/detail.html', {'cart': cart})
--- online_shop/shop/views.py
+++ online_shop/shop/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404
 
-
+from cart.forms import CartAddProductForm
 from .models import Category, Product
 
 
@@ -20,4 +20,5 @@ def product_list(request, category_slug=None):
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    return render(request, 'shop/product/detail.html', {'product': product})
+    cart_product_form = CartAddProductForm()
+    return render(request, 'shop/product/detail.html', {'product': product, 'cart_product_form': cart_product_form})
